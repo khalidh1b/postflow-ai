ef322b39bf3b6199c49072944e97614a8d4208e3
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-10 19:17:41 +0600

    Feat: Create reusable InputField component
    Adds a controlled input component integrated with react-hook-form for use in application forms

---
+++ src/components/post-generator/form-fields/input-field.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+
+interface InputFieldProps {
+  control: Control<any>;
+  name: string;
+  label: string;
+  placeholder?: string;
+}
+
+export const InputField = ({ control, name, label, placeholder }: InputFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input placeholder={placeholder} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
